30716874557ca6cce1ecf36c356bdbe8dc09e142
femorenoba <37084503+femorenoba@users.noreply.github.com>
2021-03-25 11:14:23 -0500

    Entidad room
    Se agrego la entidad room para la creacion de la tabla

---
+++ BackVisitUrRoom/src/main/java/com/backvisitur/entity/Room.java
@@ -0,0 +1,123 @@
+package com.backvisitur.entity;
+import java.time.LocalDateTime;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "rooms")
+public class Room {
+	
+	 @Id
+	 @GeneratedValue(strategy=GenerationType.AUTO)
+	 private Long id;
+	 @ManyToOne
+	 private User user; // El usuario responsable
+	 @ManyToOne
+	 private User updateUser; // El usuario que edita
+	 @ManyToOne
+	 private User ownerUser; // El usuario que creo 
+	 private LocalDateTime registerTime;  
+	 private LocalDateTime updateTime;  
+	
+	private String title;
+	private String desctription;
+	private Integer price;
+	private String uploaddate;
+	private String image;
+	
+	public Room(){
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public User getUpdateUser() {
+		return updateUser;
+	}
+
+	public void setUpdateUser(User updateUser) {
+		this.updateUser = updateUser;
+	}
+
+	public User getOwnerUser() {
+		return ownerUser;
+	}
+
+	public void setOwnerUser(User ownerUser) {
+		this.ownerUser = ownerUser;
+	}
+
+	public LocalDateTime getRegisterTime() {
+		return registerTime;
+	}
+
+	public void setRegisterTime(LocalDateTime registerTime) {
+		this.registerTime = registerTime;
+	}
+
+	public LocalDateTime getUpdateTime() {
+		return updateTime;
+	}
+
+	public void setUpdateTime(LocalDateTime updateTime) {
+		this.updateTime = updateTime;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDesctription() {
+		return desctription;
+	}
+
+	public void setDesctription(String desctription) {
+		this.desctription = desctription;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public String getUploaddate() {
+		return uploaddate;
+	}
+
+	public void setUploaddate(String uploaddate) {
+		this.uploaddate = uploaddate;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	
+
+}
